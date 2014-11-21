3449917948eead79cdc1833f3aeafd4abae22de6
Ty Cobb <ty.cobb.m@gmail.com>
2014-11-20 19:17:02 -0600

    refactored fixtures to be a bit simpler

--- server/fixtures.js
+++ server/fixtures.js
@@ -7,109 +7,47 @@ Meteor.startup(function() {
   
   //
   // Fixtures Helper
-  //
-   
-  var fixtures = {
- 
-    settings: Meteor.settings.fixtures,
+  //    
+
+  var settings = Meteor.settings.fixtures;
+
+  Mongo.Collection.prototype.fixtures = function(data) {
+    var self = this;
   
-    rebuild: function(fixturesModel) {
- 
-      // collections can be keyed on the global object
-      var collectionName  = fixturesModel.collectionName;
-      var collection      = global[capitalize(collectionName)];
-      
-      // verify that we found a colleciton and that we're supposed to build fixtures 
-      // for this collection
-      if(!collection || !this.settings.collections[collectionName]) {
-        return;    
-      }
-           
-      // purge stale data 
-      collection.remove({});
-
-      // populate each record with the defaults object (if any) and insert it
-      _.each(fixturesModel.records, function(record) {
-        record = _.defaults(record, fixturesModel.defaults);
-        collection.insert(record);
-      });
-    }
-
-  }
-
-  // if the rebuild flag is off, then we won't run any fixtures 
-  if(!fixtures.settings.rebuild) {
-    return;
-  }
-
-  // otherwise, we'll conditionally run all our fixtures on a per-collection basis
-
-  /*
-
-  fixtures.rebuild({
-
-    collectionName: 'anthologies',
-
-    records: [{
-      title: 'Miscellaneous Anthology'
-    }]
-
-  });
-
-  var anthology = Anthologies.findOne();
-
-  //
-  //Template.proposals.helpers({ Compositions 
-  //
-
-  fixtures.rebuild({
-
-    collectionName: 'compositions',  
+    // if fixture rebuilding is off (globally or for this collection), do nothing
+    if(!settings.rebuild || !settings.collections[self._name]) {
+      return;
+    } 
     
-    records: [{ 
-      anthology_id: anthology._id,
-      title: 'Hello, Composition!',
-    }, { 
-      anthology_id: anthology._id,
-      title: 'Hello, Composition 2!',
-    }]
+    // purge stale data (everything in this colleciton)
+    self.remove({});
 
-  });
+    // populate each record with the defaults object (if any) and insert it
+    data.records.forEach(function(record) {
+      record = _.defaults(record, data.defaults);
+      self.insert(record);
+    });
+  };
 
   //
-  // Listings
+  // Fixtures
   //
-  
-  var composition = Compositions.findOne();
-  
-  fixtures.rebuild({
-    
-    collectionName: 'lines', 
+
+  Stories.fixtures({ 
 
     defaults: {
-      composition_id: composition._id,
-      isProposed:     false,
-      isParagraph:    false,
-      isVisible:      true,   
+      isGreat: true
     },
-    
+
     records: [{
-      text: "This is the sample story, and it's unique in that regard alone.", 
-      order: 0,
+      title: "Ender's Game"
     },{
-      text: "While many other stories exist, few are so devoid of content as the example you see here--glance towards any of your home's many bookshelves to verify.", 
-      order: 1,
+      title: "Infinite Jest"
     },{
-      text: "Full of books, right?",
-      order: 2,
-    },{
-      text: "And what a great sample this is! Do it up.",  
-      order: 3,
-    }],
+      title: "Barbie's Horse Adventure" 
+    }]
 
   });
-  
-  */
 
 });
 
